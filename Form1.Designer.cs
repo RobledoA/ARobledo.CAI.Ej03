d5a95d20033d9c2569268878603819b93875601e
Alan <alan.robledo2021@gmail.com>
2023-09-14 19:08:18 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ARobledo.CAI.Ej03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            comboBox3 = new ComboBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Location = new Point(31, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(545, 276);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingrese los siguientes datos para continuar";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(22, 60);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(496, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 15);
+            label1.TabIndex = 3;
+            label1.Text = "País de Residencia";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Provincia/ Región";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(22, 114);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(496, 23);
+            comboBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 153);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Localidad";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(22, 171);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(496, 23);
+            comboBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(22, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(33, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Calle";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 224);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(268, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(296, 224);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(70, 23);
+            textBox2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(296, 206);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Numero";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(372, 224);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(70, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(372, 206);
+            label6.Name = "label6";
+            label6.Size = new Size(29, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Piso";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(448, 224);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(70, 23);
+            textBox4.TabIndex = 16;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(448, 206);
+            label7.Name = "label7";
+            label7.Size = new Size(33, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Dpto";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(420, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Enviar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(501, 305);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 352);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Label label3;
+        private ComboBox comboBox3;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace ARobledo.CAI.Ej03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
